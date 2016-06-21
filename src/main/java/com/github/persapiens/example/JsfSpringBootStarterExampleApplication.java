3d6d195cb15e9059c51d1fbcc426759ae8c46098
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-06-21 20:57:19 -0300

    fix #1 migrate project structure to work with war packaging.

--- src/main/java/com/github/persapiens/example/JsfSpringBootStarterExampleApplication.java
+++ src/main/java/com/github/persapiens/example/JsfSpringBootStarterExampleApplication.java
@@ -2,9 +2,18 @@ package com.github.persapiens.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JsfSpringBootStarterExampleApplication {
+public class JsfSpringBootStarterExampleApplication 
+    extends SpringBootServletInitializer
+{
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(JsfSpringBootStarterExampleApplication.class);
+    }
     
 	public static void main(String[] args) {
 		SpringApplication.run(JsfSpringBootStarterExampleApplication.class, args);
