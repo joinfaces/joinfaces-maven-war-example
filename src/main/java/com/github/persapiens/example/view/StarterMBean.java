3488376adbf104437d07fe6a54133935af0778c7
Marcelo Romulo Fernandes <persapiens@gmail.com>
2016-07-10 23:52:03 -0300

    jsf-spring-boot-starter upgraded to 1.7.0 starring butterfaces.

---
+++ src/main/java/com/github/persapiens/example/view/StarterMBean.java
@@ -0,0 +1,58 @@
+package com.github.persapiens.example.view;
+
+import java.io.Serializable;
+import javax.faces.bean.ViewScoped;
+import javax.inject.Named;
+
+@Named
+@ViewScoped
+public class StarterMBean implements Serializable {
+    
+    private String servletContainer = "Tomcat";
+
+    private String jsfImplementation = "Mojarra";
+    
+    private String jsfComponents = "PrimeFaces";
+
+    public String getServletContainer() {
+        return servletContainer;
+    }
+
+    public void setServletContainer(String servletContainer) {
+        this.servletContainer = servletContainer;
+    }
+
+    public String getJsfImplementation() {
+        return jsfImplementation;
+    }
+
+    public void setJsfImplementation(String jsfImplementation) {
+        this.jsfImplementation = jsfImplementation;
+    }
+
+    public String getJsfComponents() {
+        return jsfComponents;
+    }
+
+    public void setJsfComponents(String jsfComponents) {
+        this.jsfComponents = jsfComponents;
+    }        
+     
+    public String getStarter() {
+        String result = "";
+        
+        if(!servletContainer.equals("Tomcat")) {
+            result += "-" + servletContainer ;
+        }
+        
+        if(!jsfImplementation.equals("Mojarra")) {
+            result += "-" + jsfImplementation;
+        }
+        
+        if(!jsfComponents.equals("PrimeFaces")) {
+            result += "-" + jsfComponents;
+        }
+        
+        return "jsf" + result.toLowerCase() + "-spring-boot-starter";
+    }
+}
